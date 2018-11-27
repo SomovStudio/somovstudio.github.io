2a38eb433a713675dc9c41485f1102dbecf3bf88
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2018-11-27 10:16:06 +0300

    new design site

--- new_version/js/main.js
+++ new_version/js/main.js
@@ -1,5 +1,18 @@
-$(document).ready(function()
-{
-	
-	
+$(document).ready(function(){
+
+	$('#title').animate({opacity:1}, 2000);
+	$('#logo').animate({opacity:1}, 2000);
+	$('#info').animate({opacity:1}, 2000);
+	$('.Rotate').toggleClass('RotateBack');
+
+	$(window).scroll(function(){
+		var scrolled = $(this).scrollTop();
+		$("#headerTitleAndLogo").css({
+			"transform" : "translate(0%, " + scrolled + "%)"
+		});
+		
+		$("#contacts img").css({
+			"transform" : "translate(0%, -" + scrolled / 10.5 + "%)"
+		});
+	});
 });
