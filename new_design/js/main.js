ee3467f06d8940d985cfac896a817fec08e0e08b
Somov <somov.studio@gmail.com>
2022-07-15 09:41:10 +0300

    Удаление старых версий сайта

--- new_design/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('#logo_title').animate({opacity:1}, 2000);
-	$('#logo_image').animate({opacity:1}, 2000);
-	$('#logo_info').animate({opacity:1}, 2000);
-	$('.logo__image_rotate').toggleClass('logo__image_back');
-
-});
