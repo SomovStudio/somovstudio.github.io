19bbbc9c04b3ec975a73de12df21f7c2960d0efd
Somov Studio <somov.studio@gmail.com>
2026-03-03 13:26:31 +0300

    Макет сайта LTK

---
+++ test/LTK/assets/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // console.log('Страница загружена');
+});
+
+window.addEventListener('load', function() {
+    // console.log('Все ресурсы загружены');
+});
