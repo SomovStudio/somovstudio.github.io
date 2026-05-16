8007c33b31c632edbbd68504dac7cbcf54205ec5
Somov Studio <somov.studio@gmail.com>
2026-05-16 06:36:17 +0300

    Загружены билды на сайт для ru версии сайта

--- ru/js/main.js
+++ ru/js/main.js
@@ -10,9 +10,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
-    changeDownloadUrl(btnHatDownload1, 'https://gitflic.ru/project/somovstudio/hat/release/55779d40-257f-47d7-82f6-1d9d7d9be3d0/95718f2d-e0a0-41a3-85db-77dc75205acb/download');
-    changeDownloadUrl(btnHatDownload2, 'https://gitflic.ru/project/somovstudio/hat/release/55779d40-257f-47d7-82f6-1d9d7d9be3d0/95718f2d-e0a0-41a3-85db-77dc75205acb/download');
-    changeDownloadUrl(btnPluginDownload, 'https://gitflic.ru/project/somovstudio/hatpluginmysql/release/acf4d342-b595-4bce-8e5f-c7c7eca655ec/48404465-5308-40b5-95e5-238cb530c220/download');
+    changeDownloadUrl(btnHatDownload1, 'https://somovstudio.github.io/ru/download/Hat-1.5.2.zip');
+    changeDownloadUrl(btnHatDownload2, 'https://somovstudio.github.io/ru/download/Hat-1.5.2.zip');
+    changeDownloadUrl(btnPluginDownload, 'https://somovstudio.github.io/ru/download/HatPluginMySql-1.0.15.0.zip');
 
     function updateHatVersion(version, date) {
         const versionElement = document.getElementById('HatVersion');
